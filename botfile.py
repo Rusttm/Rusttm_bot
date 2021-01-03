24c6c2fb46a20fcfd3a19d52ca0df01ce7e59a19
Rusttm <rust_mazh@mail.ru>
2021-01-04 02:17:03 +0300

    second commit

--- botfile.py
+++ botfile.py
@@ -1,15 +1,22 @@
 
 import os
 import configparser  # импортируем библиотеку
+def path_finder():
+    return os.path.dirname(os.path.abspath(__file__))
 def write_to_config(section, name, value):
     ''' Writing data in config file. Warnings!!! it erases other data '''
-    conf = configparser.ConfigParser()  # создаём объекта парсера
-    conf[section]={name: value}
-    with open('config.ini', 'w') as configfile:
+    conf = configparser.ConfigParser()
+    conf.set(section, name, value)
+    with open('config.ini', 'ab') as configfile:
         conf.write(configfile)
+
+
 def read_from_config(section: str, key: str):
     ''' Read data from config file '''
     conf = configparser.ConfigParser()
     conf.read('config.ini')
     return conf[section][key]
-print(read_from_config('id', 'rustam'))
+
+write_to_config('telegram', 'password', '111')
+#read_from_config('telegram', 'password')
+#print(read_from_config('telegram', 'password'))
